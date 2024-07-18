a4402a913d2e0e383a0fb95302cc8788bcaecd77
Ilhammaulidra <ilhammaulidra12@gmail.com>
2024-07-18 14:30:10 +0700

    Modifikasi file style.css dan script.js untuk effect paralax

--- js/script.js
+++ js/script.js
@@ -11,7 +11,23 @@ $('.page-scroll').on('click', function(e){
     // //pindahkan scroll
     $('html, body').animate({
         scrollTop: elemenTujuan.offset().top - 50
-    }, 1500, 'easeInOutExpo');
+    }, 2000, 'easeInOutExpo');
 
     e.preventDefault();
+});
+
+//Effect Paralax
+$(window).scroll(function(){
+	// console.log('ok');
+	var wScroll = $(this).scrollTop();
+	$('.jumbotron img').css({
+		'transform' : 'translate(0px, '+ wScroll/4 +'%)'
+	});
+
+	$('.jumbotron h1').css({
+		'transform' : 'translate(0px, '+ wScroll/2 +'%)'
+	});
+    $('.jumbotron p').css({
+		'transform' : 'translate(0px, '+ wScroll/1.2 +'%)'
+	});
 });
