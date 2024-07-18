294e55dffb1fff4dab4cc6f92a1264221525cede
Ilhammaulidra <ilhammaulidra12@gmail.com>
2024-07-18 15:36:59 +0700

    Menambahkan effect pada tampilan about

--- js/script.js
+++ js/script.js
@@ -17,8 +17,16 @@ $('.page-scroll').on('click', function(e){
 });
 
 //Effect Paralax
+//About
+$(window).on('load', function(){
+    $('.pKiri').addClass('pMuncul');
+    $('.pKanan').addClass('pMuncul');
+});
+
 $(window).scroll(function(){
 	// console.log('ok');
+
+    // Jumbotron
 	var wScroll = $(this).scrollTop();
 	$('.jumbotron img').css({
 		'transform' : 'translate(0px, '+ wScroll/4 +'%)'
@@ -30,4 +38,13 @@ $(window).scroll(function(){
     $('.jumbotron p').css({
 		'transform' : 'translate(0px, '+ wScroll/1.2 +'%)'
 	});
+
+    // Portfolio
+    if(wScroll > $('.portfolio').offset().top - 250 ){
+        $('.portfolio .thumbnail').each(function(i){
+            setTimeout(function(){
+                $('.portfolio .thumbnail').eq(i).addClass('muncul');
+            }, 300 * (i+1));
+        });
+    }
 });
